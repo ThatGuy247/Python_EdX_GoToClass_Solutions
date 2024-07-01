0214ed77a3f8b2b39be6e741583becc7ada88ba2
Arman Emamhosseini <75748147+ThatGuy247@users.noreply.github.com>
2024-07-01 12:44:42 +0330

    Update Part3 - find_longest_word.py

--- Quiz-4/Part3 - find_longest_word.py
+++ Quiz-4/Part3 - find_longest_word.py
@@ -2,7 +2,7 @@ def find_longest_word(input_string):
     longest_word =  max(input_string.split(' '), key=len)
     a = len(longest_word)
     l = []
-    for item in input_string.split(' '):
+    for item in input_string.split(' '): 
         if len(item) == a:
             l.append(item)
     return str(l[-1])
